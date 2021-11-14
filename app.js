eec26c495919db7a88408e8b623852c27be54692
munibafareed <fmuniba34@gmail.com>
2021-11-14 15:57:13 -0800

    work complete

---
+++ app.js
@@ -0,0 +1,37 @@
+// date method,,,Q # 1,,,
+
+// var now = new Date();
+// console.log(now)
+
+// Q # 2,,,current month,,,,
+ 
+// var now =new Date ();
+// console.log(now.getMonth());
+
+// Q # 3,,,,string,,,,kisi b cheez ki datatype change,,
+
+// var now = new Date().toString();
+// console.log(now.slice(0,3));
+
+// Q # 6 ,,,
+
+// var now = new Date ();
+// console.log(now)
+
+// var now = new Date ();
+// console.log(now.getTime());
+
+// var now = new Date ();
+// console.log(now.getMinutes());
+
+
+// Q # 7,,,,hours,,,
+// var now = new Date();
+// console.log(now.getHours());
+// alert(now)
+
+// Q # 8,,,,last day of month,,, 
+
+var now = new Date();
+console.log(now);
+
